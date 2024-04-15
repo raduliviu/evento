e9d62db3b09e11833e991e1653dc2a47e18ee7a0
Radu Baltag <baltag.radu@gmail.com>
2024-04-15 19:08:04 +0200

    all events page fetching from api

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,3 +1,26 @@
-export default function EventsPage() {
-  return <main>EventsPage</main>;
+import H1 from '@/components/h1';
+import { EventoEventType } from '@/lib/types';
+
+type EventsPageProps = {
+  params: { city: string };
+};
+
+export default async function EventsPage({ params }: EventsPageProps) {
+  const city = params.city;
+  const capitalizedCity = city.charAt(0).toUpperCase() + city.slice(1);
+
+  const response = await fetch(
+    'https://bytegrad.com/course-assets/projects/evento/api/events?city=austin'
+  );
+  const events: EventoEventType[] = await response.json();
+
+  return (
+    <main className='flex flex-col items-center py-24 px-[20px] min-h-[110vh]'>
+      <H1>{city === 'all' ? 'All Events' : `Events in ${capitalizedCity}`}</H1>
+
+      {events.map((event) => (
+        <section key={event.id}>{event.name}</section>
+      ))}
+    </main>
+  );
 }
