7ea52bbb5a90e7198ed7689c9a9a797a40c409f0
Radu Baltag <baltag.radu@gmail.com>
2024-04-23 18:41:46 +0200

    moved getter functions into server only file

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,5 @@
 import clsx, { ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
-import prisma from './db';
-import { notFound } from 'next/navigation';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -9,41 +7,4 @@ export function cn(...inputs: ClassValue[]) {
 
 export function capitalizeWord(word: string) {
   return word.charAt(0).toUpperCase() + word.slice(1);
-}
-
-export async function getEvents(city: string, page = 1) {
-  const events = await prisma.eventoEvent.findMany({
-    where: {
-      city: city === 'all' ? undefined : capitalizeWord(city),
-    },
-    orderBy: {
-      date: 'asc',
-    },
-    take: 6,
-    skip: (page - 1) * 6,
-  });
-
-  let totalCount;
-  if (city === 'all') {
-    totalCount = await prisma.eventoEvent.count();
-  } else {
-    totalCount = await prisma.eventoEvent.count({
-      where: { city: capitalizeWord(city) },
-    });
-  }
-
-  return { events, totalCount };
-}
-
-export async function getEvent(slug: string) {
-  const event = await prisma.eventoEvent.findUnique({
-    where: {
-      slug: slug,
-    },
-  });
-
-  if (!event) {
-    return notFound();
-  }
-  return event;
 }
